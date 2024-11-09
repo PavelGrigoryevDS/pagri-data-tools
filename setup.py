bfac09fc04da196b1258afec145bc9e5bb6d3bc8
Pavel Grigoryev <pagri.analytics@gmail.com>
2024-11-09 07:14:23 +0300

    [main] at 2024-11-09 07:14:23 with changes: setup.py prev commit: bb7aba9

--- setup.py
+++ setup.py
@@ -6,20 +6,9 @@ setup(
     packages=find_packages(),  # Найти все пакеты
     install_requires=[
         'dash==2.18.2'
-        'ipython==8.27.0'
-        'ipywidgets==8.1.5'
-        'nbformat==5.10.4'
-        'numpy==2.1.3'
-        'pandas==2.2.3'
         'pingouin==0.5.5'
-        'plotly==5.24.1'
         'pyaspeller==2.0.0'
         'pymystem3==0.2.0'
-        'scikit_learn==1.5.2'
-        'scipy==1.14.1'
-        'statsmodels==0.14.4'
-        'termcolor==2.5.0'
-        'tqdm==4.67.0'
         ],
     description='This repository contains data analysis modules, including tools for data preprocessing, visualization, statistical analysis.',  # Описание
     long_description=open('README.md').read(),  # Долгое описание из README
